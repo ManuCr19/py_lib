89753614e8ef26b9858f71f15732c676169c97cb
manucr19 <manuelcambon19@gmail.com>
2023-04-15 22:43:02 +0200

    Cambiar el nombre del paquete por fallos a la hora de importarlo en otros archivos

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='manucr19-py',
+    name='manucr19_py',
     version='0.1',
     url='https://github.com/manucr19/library',
     packages=find_packages(),
