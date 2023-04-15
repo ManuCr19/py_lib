be47bd007e8046095b938228a0147d537466a83a
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2023-04-15 04:57:39 +0200

    Cambio en los comentarios y sustituir os.remove

--- file.py
+++ file.py
@@ -1,30 +1,39 @@
+from subprocess import run
+
 def cat(file):
-    # 'file': "/ruta/ejemplo.txt"
     # Muestra por pantalla el contenido del fichero 'file'
+    #
+    # file: str - Ruta del fichero (Ej:"/ruta/ejemplo")
+    #
     f = open(file,"r")
     txt = f.read()
     print(txt)
     f.close()
 
 def create(file,txt):
-    # 'file': "/ruta/ejemplo.txt"
-    # 'txt': "Texto de ejemplo"
     # Crea o sobreescribe el fichero 'file' con el string 'txt'
+    #
+    # file: str - Ruta del fichero (Ej:"/ruta/ejemplo.txt")
+    # txt: str - Contenido que se escribirá en el fichero
+    #
     f = open(file,"w")
     f.write(txt)
     f.close()
 
 def delete(file):
-    # 'file': "/ruta/ejemplo.txt"
     # Elimina el fichero 'file'
-    from os import remove
-    remove(file)
+    #
+    # file: str - Ruta del fichero (Ej:"/ruta/ejemplo.txt")
+    #
+    run(["rm","-f",file])
 
 def replace_text(file,str1,str2):
-    # 'file': "/ruta/ejemplo.txt"
-    # 'str1': "Texto de ejemplo"
-    # 'str2': "Texto de ejemplo"
     # Remplaza las apariciones del texto 'str1' por el texto 'str2' en el fichero 'file'
+    #
+    # file: str - Ruta del fichero (Ej:"/ruta/ejemplo.txt")
+    # str1: str - Texto original que será reemplazado
+    # str2: str - Texto nuevo que reemplazará al texto original
+    #
     f = open(file,"r")
     txt = f.read()
     txt = txt.replace(str1,str2)
@@ -34,9 +43,12 @@ def replace_text(file,str1,str2):
     f.close()
 
 def add_text(file,txt):
-    # 'file': "/ruta/ejemplo.txt"
-    # 'txt': "Texto de ejemplo"
     # Añade el texto 'txt' al final del fichero 'file'
+    #
+    # file: str - Ruta del fichero (Ej:"/ruta/ejemplo.txt")
+    # txt: str - Contenido que se añadirá al final del fichero
+    #
     f = open(file,"a")
     f.write("\n" + txt)
     f.close()
+
