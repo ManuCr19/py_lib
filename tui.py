b7df88a820ccf443f04569ff0d240528a4f49620
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2023-04-15 04:40:47 +0200

    Comentar funciones

--- tui.py
+++ tui.py
@@ -38,6 +38,16 @@ def yesno(title, text, height = None, width = None):
         exit()
 
 def inputbox(title, text, init = "", height = None, width = None):
+    # Muestra una ventana con un campo de entrada de texto, con título y texto personalizados
+    #
+    # title: str - Texto que se mostrará como título de la ventana
+    # text: str - Texto que se mostrará en el contenido de la ventana
+    # init: str (opcional) - Texto que se mostrará como valor inicial del campo de entrada de texto
+    # height: int (opcional) - Número de píxeles para definir la altura de la ventana
+    # width: int (opcional) - Número de píxeles para definir el ancho de la ventana
+    #
+    # returns: str - Texto ingresado por el usuario
+    #
     text = "\n" + text
     d = Dialog(dialog = "dialog", autowidgetsize = True)
     code, string = d.inputbox(text, init = init, title = title, height = height, width = width)
@@ -51,6 +61,17 @@ def inputbox(title, text, init = "", height = None, width = None):
         exit()
 
 def menu(title, text, choices, height = None, width = None):
+    # Muestra una ventana con una lista de opciones, con título y texto personalizados
+    #
+    # title: str - Texto que se mostrará como título de la ventana
+    # text: str - Texto que se mostrará en el contenido de la ventana
+    # choices: list of tuples - Lista de opciones como tuplas (tag, descripción)
+    #                            Ejemplo: [("1.", "Opción 1"), ("2.", "Opción 2")]
+    # height: int (opcional) - Número de píxeles para definir la altura de la ventana
+    # width: int (opcional) - Número de píxeles para definir el ancho de la ventana
+    #
+    # returns: str - El tag de la opción seleccionada por el usuario (Por ejemplo: "1." O "2.")
+    #
     text = "\n" + text
     d = Dialog(dialog = "dialog", autowidgetsize = True)
     code, tag = d.menu(text, choices = choices, title = title, height = height, width = width)
@@ -64,6 +85,13 @@ def menu(title, text, choices, height = None, width = None):
         exit()
 
 def infobox(title, text, height = None, width = None):
+    # Muestra una ventana con información en formato de caja, con título y texto personalizados
+    #
+    # title: str - Texto que se mostrará como título de la ventana
+    # text: str - Texto que se mostrará en el contenido de la ventana
+    # height: int (opcional) - Número de píxeles para definir la altura de la ventana
+    # width: int (opcional) - Número de píxeles para definir el ancho de la ventana
+    #
     text = "\n" + text
     d = Dialog(dialog = "dialog", autowidgetsize = True)
     code = d.infobox(text, title = title, height = height, width = width)
