7e1221da714c3a37fa03eec3cc48f3430dad204b
manucr19 <manuelcambon19@gmail.com>
2023-04-15 22:29:43 +0200

    Añadir comas en el fichero setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='manucr19-py',
     version='0.1',
-    url='https://github.com/manucr19/library'
+    url='https://github.com/manucr19/library',
     packages=find_packages(),
-    license='GPLv3'
+    license='GPLv3',
     install_requires=[
         'dialog',
         'subprocess'
