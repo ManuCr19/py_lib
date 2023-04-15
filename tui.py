903d5c24367e7cd46e941e16b58956819b4c9e92
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2023-04-15 04:22:31 +0200

    Comentar funciones

--- tui.py
+++ tui.py
@@ -2,6 +2,13 @@ from dialog import Dialog
 from subprocess import run
 
 def msgbox(title, text, height = None, width = None):
+    # Muestra una ventana de mensaje con título y texto personalizados
+    #
+    # title: str - Texto que se mostrará como título de la ventana
+    # text: str - Texto que se mostrará en el contenido de la ventana
+    # height: int (opcional) - Número de píxeles para definir la altura de la ventana
+    # width: int (opcional) - Número de píxeles para definir el ancho de la ventana
+    #
     text = "\n" + text
     d = Dialog(dialog = "dialog", autowidgetsize = True)
     code = d.msgbox(text, title = title, height = height, width = width)
@@ -10,6 +17,15 @@ def msgbox(title, text, height = None, width = None):
         exit()
 
 def yesno(title, text, height = None, width = None):
+    # Muestra una ventana con opciones de sí/no, con título y texto personalizados
+    #
+    # title: str - Texto que se mostrará como título de la ventana
+    # text: str - Texto que se mostrará en el contenido de la ventana
+    # height: int (opcional) - Número de píxeles para definir la altura de la ventana
+    # width: int (opcional) - Número de píxeles para definir el ancho de la ventana
+    #
+    # returns: bool - True si el usuario selecciona "Sí", False si selecciona "No"
+    #
     text = "\n" + text
     d = Dialog(dialog = "dialog", autowidgetsize = True)
     code = d.yesno(text, title = title, height = height, width = width)
