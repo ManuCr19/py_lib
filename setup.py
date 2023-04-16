3398ba72f5b0efd98a7b14f02142705435d801f1
manucr19 <manuelcambon19@gmail.com>
2023-04-16 18:15:59 +0200

    Actualizar numero version por el HotFix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manucr19_py',
-    version='1.0',
+    version='1.0.1',
     url='https://github.com/manucr19/library',
     packages=find_packages(),
     license='GPLv3',
